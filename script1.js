70f72d811b2e7f0a1947ab7ba2706e387b3f5719
klNarayanaRao <95900325+klNarayanaRao@users.noreply.github.com>
2023-06-18 21:57:56 +0530

    script1.js

--- script1.js
+++ script1.js
@@ -7,32 +7,30 @@ const options = {
 };
 
 try {
-    let city = document.querySelector('#city-text')
-    let cityname = document.querySelector('#city-name')
-    let button = document.querySelector('#search-button')
-    
-    // updating data in card when "Search" button is clicked
-    button.addEventListener('click',async () => {
-        console.log('button clicked')
-        console.log('city.value')
-        let resultData = await fetchdata(city.value);
-        displaySearchData(resultData);
-        cityname.textContent = city.value // update heading
-    })
+  let city = document.querySelector("#city-text");
+  let cityname = document.querySelector("#city-name");
+  let button = document.querySelector("#search-button");
+
+  // updating data in card when "Search" button is clicked
+  button.addEventListener("click", async () => {
+    let resultData = await fetchdata(city.value);
+    displaySearchData(resultData);
+    cityname.textContent = city.value; // update heading
+    city.value = ""; // clearing input value
+  });
 } catch (error) {
   console.error(error);
 }
 
 // Frequent Cities Code
-const frequentCities = ['Delhi', 'Kolkata', 'Mumbai', 'Hyderabad']
-const frequentCityTable = document.querySelector('#frequent-city-table')
+const frequentCities = ["Delhi", "Kolkata", "Mumbai", "Hyderabad"];
+const frequentCityTable = document.querySelector("#frequent-city-table");
 
 function getFrequentCityData() {
   frequentCities.forEach(async (cityName) => {
     let results = await fetchdata(cityName);
-    updateTable(cityName, results)
-
-  })
+    updateTable(cityName, results);
+  });
 }
 
 function updateTable(cityName, results) {
@@ -48,29 +46,33 @@ function updateTable(cityName, results) {
   <td>${results["wind_speed"]}</td>
   <td>${results["wind_degrees"]}°</td>
   `;
-
 }
-async function fetchdata (city = 'Delhi'){
-    // modular function to get the JSON data from the API
-    const response = await fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`, options);
-    const result = await response.json();
-    
-    
-    return result
-}
+async function fetchdata(city = "Delhi") {
+  // modular function to get the JSON data from the API
+  const response = await fetch(
+    `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`,
+    options
+  );
+  const result = await response.json();
 
+  return result;
+}
 
 function displaySearchData(result) {
   // this function updates the card's details
-  document.querySelector('#temp').textContent = result['temp']
-  document.querySelector('#temp2').textContent = result['temp'];
-  document.querySelector('#feels_like').textContent = result['feels_like']
-  document.querySelector('#humidity').textContent = result['humidity']
-  document.querySelector('#min_temp').textContent = result['min_temp']
-  document.querySelector('#max_temp').textContent = result['max_temp']
-  document.querySelector('#wind_speed').textContent = result['wind_speed']
-  document.querySelector('#wind_degrees').textContent = result['wind_degrees']
-  document.querySelector('#wind_speed2').textContent = result['wind_speed'];
+  document.querySelector("#temp").textContent = result["temp"];
+  document.querySelector("#temp2").textContent = result["temp"];
+  document.querySelector("#feels_like").textContent = result["feels_like"];
+  document.querySelector("#humidity").textContent = result["humidity"];
+  document.querySelector("#min_temp").textContent = result["min_temp"];
+  document.querySelector("#max_temp").textContent = result["max_temp"];
+  document.querySelector("#wind_speed").textContent = result["wind_speed"];
+  document.querySelector("#wind_degrees").textContent = result["wind_degrees"];
+  document.querySelector("#wind_speed2").textContent = result["wind_speed"];
 }
 
 getFrequentCityData();
+
+function handleSubmit(event) {
+  event.preventDefault();
+}
